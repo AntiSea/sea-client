7c6b89333669103365b3fd50a2d132bf7cbde312
unknownsea <seasoank@gmail.com>
2025-03-13 17:36:48 -0700

    Testing ui
    Might main imgui for this client

--- src/main/java/net/antisea/seaclient/SeaClient.java
+++ src/main/java/net/antisea/seaclient/SeaClient.java
@@ -1,33 +1,57 @@
 package net.antisea.seaclient;
 
+import net.antisea.seaclient.gui.Render;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.metadata.ModMetadata;
+import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class SeaClient implements ClientModInitializer {
-	public static final String MOD_ID = "sea-client";
-	public static final ModMetadata MOD_META;
-	public static final String NAME;
-	public static final String BUILD_NUMBER;
-	public static final Logger LOG;
+    public static final String MOD_ID = "sea-client";
+    public static final ModMetadata MOD_META;
+    public static final String NAME;
+    public static final String BUILD_NUMBER;
+    public static final Logger LOG;
 
     static {
-		MOD_META = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata();
-		NAME = MOD_META.getName();
-		LOG = LoggerFactory.getLogger(NAME);
-		String versionString = MOD_META.getVersion().getFriendlyString();
-		if (versionString.contains("-")) versionString = versionString.split("-")[0];
-		String buildNumber = "unknown";
-		if (MOD_META.containsCustomValue(SeaClient.MOD_ID + ":build_number")) {
-		    buildNumber = MOD_META.getCustomValue(SeaClient.MOD_ID + ":build_number").getAsString();
-		}
+        MOD_META = FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow().getMetadata();
+        NAME = MOD_META.getName();
+        LOG = LoggerFactory.getLogger(NAME);
+        String versionString = MOD_META.getVersion().getFriendlyString();
+        if (versionString.contains("-")) versionString = versionString.split("-")[0];
+        String buildNumber = "unknown";
+        if (MOD_META.containsCustomValue(SeaClient.MOD_ID + ":build_number")) {
+            buildNumber = MOD_META.getCustomValue(SeaClient.MOD_ID + ":build_number").getAsString();
+        }
         BUILD_NUMBER = buildNumber;
     }
 
     @Override
     public void onInitializeClient() {
         LOG.info("Initializing {}", NAME);
+        LOG.info("Version: {} (build {})", MOD_META.getVersion().getFriendlyString(), BUILD_NUMBER);
+
+        // Register a client tick event to initialize ImGui and handle key input
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            long windowHandle = GLFW.glfwGetCurrentContext();
+            if (windowHandle != 0) {
+                Render.init(windowHandle);
+            } else {
+                LOG.error("Failed to get GLFW window handle");
+            }
+
+            if (GLFW.glfwGetKey(windowHandle, GLFW.GLFW_KEY_RIGHT_SHIFT) == GLFW.GLFW_PRESS) {
+                Render.toggleGui();
+            }
+        });
+
+        // Register a world render event to render ImGui
+        WorldRenderEvents.END.register(context -> {
+            Render.render();
+        });
     }
 }
---
+++ src/main/java/net/antisea/seaclient/gui/Render.java
@@ -0,0 +1,92 @@
+package net.antisea.seaclient.gui;
+
+import imgui.ImGui;
+import imgui.ImGuiIO;
+import imgui.flag.ImGuiConfigFlags;
+import imgui.flag.ImGuiWindowFlags;
+import imgui.gl3.ImGuiImplGl3;
+import imgui.glfw.ImGuiImplGlfw;
+import org.lwjgl.glfw.GLFW;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Render {
+    private static final ImGuiImplGlfw imGuiGlfw = new ImGuiImplGlfw();
+    private static final ImGuiImplGl3 imGuiGl3 = new ImGuiImplGl3();
+    private static boolean initialized = false;
+    private static boolean showGui = false;
+    private static final Logger LOG = LoggerFactory.getLogger(Render.class);
+
+    public static void init(long windowHandle) {
+        if (initialized) return;
+
+        // Initialize ImGui
+        ImGui.createContext();
+        ImGuiIO io = ImGui.getIO();
+        io.addConfigFlags(ImGuiConfigFlags.NavEnableKeyboard); // Enable Keyboard Controls
+        io.addConfigFlags(ImGuiConfigFlags.DockingEnable); // Enable Docking
+        io.addConfigFlags(ImGuiConfigFlags.ViewportsEnable); // Enable Multi-Viewport / Platform Windows
+
+        imGuiGlfw.init(windowHandle, true);
+        imGuiGl3.init("#version 150");
+
+        initialized = true;
+    }
+
+    public static void render() {
+        if (!initialized || !showGui) return;
+
+        // Start the ImGui frame
+        imGuiGlfw.newFrame();
+        ImGui.newFrame();
+
+        // Render your GUI
+        ImGui.setNextWindowSize(ImGui.getIO().getDisplaySizeX(), ImGui.getIO().getDisplaySizeY());
+        ImGui.setNextWindowBgAlpha(0.5f); // Set window background transparency
+        ImGui.begin("Sea Client GUI", ImGuiWindowFlags.NoResize | ImGuiWindowFlags.NoMove | ImGuiWindowFlags.NoCollapse | ImGuiWindowFlags.NoTitleBar | ImGuiWindowFlags.NoBackground);
+
+        ImGui.text("This is a simple example of ImGui integration.");
+        if (ImGui.button("Log Info")) {
+            LOG.info("Button clicked: Log Info");
+        }
+        if (ImGui.button("Log Warning")) {
+            LOG.warn("Button clicked: Log Warning");
+        }
+        if (ImGui.button("Log Error")) {
+            LOG.error("Button clicked: Log Error");
+        }
+
+        ImGui.end();
+
+        // Render ImGui
+        ImGui.render();
+        imGuiGl3.renderDrawData(ImGui.getDrawData());
+
+        // Update and Render additional Platform Windows
+        if (ImGui.getIO().hasConfigFlags(ImGuiConfigFlags.ViewportsEnable)) {
+            final long backupWindowPtr = GLFW.glfwGetCurrentContext();
+            ImGui.updatePlatformWindows();
+            ImGui.renderPlatformWindowsDefault();
+            GLFW.glfwMakeContextCurrent(backupWindowPtr);
+        }
+    }
+
+    public static void toggleGui() {
+        showGui = !showGui;
+        if (showGui) {
+            GLFW.glfwSetInputMode(GLFW.glfwGetCurrentContext(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_NORMAL);
+        } else {
+            GLFW.glfwSetInputMode(GLFW.glfwGetCurrentContext(), GLFW.GLFW_CURSOR, GLFW.GLFW_CURSOR_DISABLED);
+        }
+    }
+
+    public static void dispose() {
+        if (!initialized) return;
+
+        imGuiGl3.dispose();
+        imGuiGlfw.dispose();
+        ImGui.destroyContext();
+
+        initialized = false;
+    }
+}
